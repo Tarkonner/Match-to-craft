0e6e80dbba1ca97b488df71d9f77f7fb87a12921
Anders Tornbjerg <anderslykketornbjerg@gmail.com>
2022-09-07 10:05:13 +0200

    Background music

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -6,6 +6,11 @@ public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance { get; private set; }
 
+
+    private AudioSource audioSource;
+    [SerializeField] AudioClip[] songs;
+    private int currentSong = -1;
+
     private void Awake()
     {
         if(Instance == null)
@@ -15,6 +20,31 @@ public class SoundManager : MonoBehaviour
         }
         else
             Destroy(gameObject);
+
+        audioSource = GetComponent<AudioSource>();
     }
 
+    private void Start()
+    {
+        currentSong = Random.Range(0, songs.Length);
+        audioSource.clip = songs[currentSong];
+        audioSource.Play();
+
+        StartCoroutine(PlayNextSong(audioSource.clip.length));
+    }
+
+    private IEnumerator PlayNextSong(float songTime)
+    {
+        yield return new WaitForSeconds(songTime);
+        int newSong = Random.Range(0, songs.Length);
+
+        while(newSong == currentSong)
+            newSong = Random.Range(0, songs.Length);
+
+        audioSource.clip = songs[newSong];
+        audioSource.Play();
+        currentSong = newSong;
+
+        StartCoroutine(PlayNextSong(audioSource.clip.length));
+    }
 }
